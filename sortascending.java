d40510afcf28d6de1d7a2be729806d263ec74004
Abdul-Rahaman-7 <168440301+Abdul-Rahaman-7@users.noreply.github.com>
2024-07-28 22:40:50 +0530

    Add files via upload

---
+++ sortascending.java
@@ -0,0 +1,15 @@
+
+import java.util.Arrays;
+
+public class sortascending {
+    public static void main( String [] args){
+        int [] no ={1,6,5,9,4,2};
+
+        Arrays.sort(no);
+
+        for(int arr : no ){
+            System.out.print(arr + " ");
+        }
+    }
+    
+}
