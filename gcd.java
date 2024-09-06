e8c7dbaef51065ffe7329061f8eb25833d2d3386
Abdul-Rahaman-7 <168440301+Abdul-Rahaman-7@users.noreply.github.com>
2024-09-06 23:21:37 +0530

    Add files via upload

---
+++ gcd.java
@@ -0,0 +1,15 @@
+public class gcd {
+    public static void main(String [] args){
+        int a=10;
+        int b=120;
+        int hcf=0;
+
+        for(int i=1;i<=a || i<=b;i++){
+            if(a%i==0 && b%i==0){
+                hcf=i;
+            }
+        }
+        System.out.println("The GCD is "+hcf);
+    }
+}
+    
